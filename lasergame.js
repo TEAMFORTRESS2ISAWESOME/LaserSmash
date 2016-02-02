27a68d9d24b804469b906b14adfd09de0700465b
TEAMFORTRESS2ISAWESOME <keithcheung@Keiths-MacBook-Pro.local>
2016-02-02 16:48:58 +0800

    index.html
    Linking the javascript with html

--- lasergame.js
+++ lasergame.js
@@ -1,3 +1,8 @@
 alert("Welcome to LaserSmash!")
 var person = prompt("Enter your nickname", "Your name here")
-x
+var r = confirm("Click Start!");
+if (r == true) {
+	x = "Stuff Happened when you pressed start"
+} else {
+	x="Stuff also happened when you pressed."
+}
