ee720623e881f3d601ded49dc84800b003b041c7
TEAMFORTRESS2ISAWESOME <keithcheung@Keiths-MacBook-Pro.local>
2016-02-02 16:52:04 +0800

    question1
    question1

--- lasergame.js
+++ lasergame.js
@@ -1,8 +1,4 @@
 alert("Welcome to LaserSmash!")
 var person = prompt("Enter your nickname", "Your name here")
-var r = confirm("Click Start!");
-if (r == true) {
-	x = "Stuff Happened when you pressed start"
-} else {
-	x="Stuff also happened when you pressed."
-}
+var r = confirm("Game Start!");
+var question = prompt("A rooster lays an egg on a roof. Which side does it roll?")
