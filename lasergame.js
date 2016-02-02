51ec0003457ecd5217692f85aed571f0179ed8ac
TEAMFORTRESS2ISAWESOME <keithcheung@Keiths-MacBook-Pro.local>
2016-02-02 17:02:51 +0800

    answer 1
    the answer

--- lasergame.js
+++ lasergame.js
@@ -1,4 +1,14 @@
 alert("Welcome to LaserSmash!")
 var person = prompt("Enter your nickname", "Your name here")
 var r = confirm("Game Start!");
-var question = prompt("A rooster lays an egg on a roof. Which side does it roll?")
+
+
+var question = prompt("A rooster lays an egg on a roof. Which side does it roll?","");
+if (question ==="answer1") {
+	alert("correct")
+}
+
+else {
+	alert("wrong")
+}
+
