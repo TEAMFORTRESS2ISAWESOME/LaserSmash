3497c481ced2bf5a77494b615b0ca9a71bcef65c
TEAMFORTRESS2ISAWESOME <keithcheung@Keiths-MacBook-Pro.local>
2016-02-16 17:01:51 +0800

    more riddles

--- lasergame.js
+++ lasergame.js
@@ -29,3 +29,21 @@ if (question3 ==="a stapler") {
 else {
 	alert("So stupid! I bet you said a vampire. Jeez! Ever thought of household objects like a STAPLER?")
 }
+
+var question4 = prompt("A man rode in to town on Tuesday, and left 2 days later on Tuesday. How so?")
+if (question4 ==="The man was in cyrogenic freezing and waited until someone invented a time machine so he could travel back in time.") {
+	alert("Now I think you're pretty good at this.")
+}
+
+else {
+	alert("I bet you said his horse was called Tuesday if you thought you were smart. WRONG! It pains me that you suck at this game.")
+}
+
+var question5 = prompt("Why did the man throw a clock out of the window?")
+if (question5 ==="Because he was an idiot") {
+	alert("Great job, rookie. I think you're ok now.")
+}
+
+else {
+	alert("YOU SUCK. You probably wrote to see time fly. Please quit this game.")
+}
