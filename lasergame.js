52e229e17a727a4fb019842711f36d6acce6bf8b
TEAMFORTRESS2ISAWESOME <keithcheung@Keiths-MacBook-Pro.local>
2016-02-16 15:33:37 +0800

    Skull craziness

--- lasergame.js
+++ lasergame.js
@@ -1,16 +1,15 @@
-alert("Welcome to LaserSmash!")
+alert("Welcome to LaserSmash!");
 var person = prompt("Enter your nickname", "Your name here")
 var r = confirm("Game Start!");
 
 
 var question = prompt("A rooster lays an egg on a roof. Which side does it roll?","");
 if (question ==="roosters don't lay eggs") {
-	alert("Correct! You got lucky this time. Let's see how you handle your next question. Here is a toucan as a reward. ")
+	alert("Correct! You got lucky this time. Let's see how you handle your next question. ");
 }
 
 else {
-	alert("You are very stupid. Roosters don't lay eggs! Quit this game and learn some stuff.
-")
+	alert("You are very stupid. Roosters don't lay eggs! Quit this game and learn some stuff.")
 }
 
 var question2 = prompt("What is heavier, a ton of feathers or a ton of gold?")
